518fa27c594499e7ef1de7a20362d17747516902
Asliddin Amirov <asliddinamir2@gmail.com>
2023-10-08 17:10:20 +0800

    last commit

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ const toggleBtn = document.querySelector('.toggle');
 const priceElements = document.querySelectorAll('.card h1');
 
 toggleBtn.addEventListener('click', () => {
-    toggleBtn.classList.toggle('right');
+    toggleBtn.classList.toggle('left');
 
-    if (toggleBtn.classList.contains('right')) {
-        priceElements.forEach((element) => {
-            element.textContent = '$199.99';
-            element.textContent = '$299.99'
-        });
+    if (toggleBtn.classList.contains('left')) {
+        priceElements[0].textContent = '$199.99';
+        priceElements[1].textContent = '$249.99';
+        priceElements[2].textContent = '$399.99';
     } else {
-        priceElements.forEach((element) => {
-            element.textContent = '$19.99';
-        });
+        priceElements[0].textContent = '$19.99';
+        priceElements[1].textContent = '$24.99';
+        priceElements[2].textContent = '$39.99';
     }
 });
